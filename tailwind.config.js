7bd572695fabb343f4c459889fa2567aab8bd029
Maarten Rischen <maartenrischen@gmail.com>
2025-04-10 15:27:26 -0300

    Remove custom Tailwind plugin causing CSS parse error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 // Add a custom plugin for 3D transforms
-const plugin = require('tailwindcss/plugin')
+// const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   darkMode: ["class"],
@@ -88,7 +88,7 @@ module.exports = {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
   		},
-  		// Add utilities for 3D transforms
+  		// Keep rotate utility for convenience
   		rotate: {
   			'y-180': '180deg'
   		},
@@ -96,22 +96,6 @@ module.exports = {
   },
   plugins: [
     require("tailwindcss-animate"),
-    // Add the custom plugin
-    plugin(function({ addUtilities }) {
-      addUtilities({
-        '.transform-style-3d': {
-          'transform-style': 'preserve-3d',
-        },
-        '.backface-hidden': {
-          'backface-visibility': 'hidden',
-        },
-        '[perspective\:1000px]': {
-          'perspective': '1000px',
-        },
-        '.rotate-y-180': {
-           transform: 'rotateY(180deg)',
-        }
-      })
-    })
+    // Custom plugin REMOVED
   ],
 }

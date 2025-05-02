233b2ebbd6510bff8f5d6d2248e8befb716b357b
Maarten Rischen <maartenrischen@gmail.com>
2025-05-01 23:14:04 -0500

    Add favicon configuration and update manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,12 @@ export default function RootLayout({
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#ffffff" />
+          
+          {/* Favicon tags */}
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         </head>
         <body className={inter.className}>
           <Toaster richColors position="top-left" />

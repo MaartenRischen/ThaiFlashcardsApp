b9238b4970369720a99bef1ef715d1ff5a8fd295
Maarten Rischen <maartenrischen@gmail.com>
2025-05-05 22:51:37 -0500

    feat: Add developer holiday note to onboarding

--- components/SetWizard/WelcomeStep.tsx
+++ components/SetWizard/WelcomeStep.tsx
@@ -52,6 +52,18 @@ export function WelcomeStep({ onNext }: { onNext: () => void }) {
           </div>
         )}
         
+        {/* Developer Note */}
+        <div className="p-4 border-2 border-orange-600/80 rounded-lg bg-orange-900/30 text-orange-200 text-sm my-4 mx-2 shadow-lg">
+          <p className="font-semibold mb-2">Note from the developer:</p>
+          <p>
+            I&apos;m on a holiday deep in the Amazon for five days until May 10. Not bringing any screens or devices, have been staring at them enough the past months and wanna stare at my wife instead. Decided to keep the app online for beta testing, but it&apos;s probably gonna break before I&apos;m back.<br /><br />
+            So if your set creation doesn&apos;t work, if the generated pictures aren&apos;t funny or return placeholder images, if the app starts to insult your ancestors for no apparant reason; don&apos;t take it too personally and just wait a couple days for me to resurface and fix it. Maybe even put the screen away as well for now. Sabai sabai.<br /><br />
+            And if the app magically keeps working without bugs: publish your best sets in the User Gallery! That&apos;d make me very happy on return from the jungle.<br /><br />
+            Gracias - or however you say that in Thai -,<br />
+            Maarten
+          </p>
+        </div>
+        
         <p className="text-xs text-gray-500 italic">
           You can skip this and use the default set or import public sets anytime.
         </p>

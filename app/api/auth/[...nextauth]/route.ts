f61805f6b996bd9112dc43769af3852330f617dd
Maarten Rischen <maartenrischen@gmail.com>
2025-05-19 13:20:20 -0600

    chore: remove NextAuth files to use Clerk exclusively

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import { handlers } from "@/app/lib/auth";
-
-export const { GET, POST } = handlers;
-
-// Force dynamic rendering for auth route to avoid caching issues
-export const dynamic = "force-dynamic"; 

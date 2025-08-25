a0153c7e603ceab06c826ac2919d367b17596bc4
Maarten Rischen <maartenrischen@gmail.com>
2025-08-25 20:08:59 +0700

    fix: convert function declaration to arrow function to resolve ES5 strict mode error

--- app/components/Confetti.tsx
+++ app/components/Confetti.tsx
@@ -16,9 +16,9 @@ export default function Confetti({ trigger, onComplete }: ConfettiProps) {
       const animationEnd = Date.now() + duration;
       const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 1000 };
 
-      function randomInRange(min: number, max: number) {
+      const randomInRange = (min: number, max: number) => {
         return Math.random() * (max - min) + min;
-      }
+      };
 
       const interval: NodeJS.Timeout = setInterval(function() {
         const timeLeft = animationEnd - Date.now();
